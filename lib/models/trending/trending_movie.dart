b23afe5465c7aa65cfa839f76d75b47bbbe522a2
Rashpinder Singh <dev.rashpinder@gmail.com>
2022-05-12 15:33:12 +0530

    Changes Made : Model Changes : 1. Added the trending Movies Response Model

---
+++ lib/models/trending/trending_movie.dart
@@ -0,0 +1,21 @@
+
+// class TrendingMoviesResponse {
+// const factory TrendingMoviesResponse({
+//     required String id,
+//     required String username,
+   
+    
+//   }) = TrendingMoviesResponse$;
+
+//   const TrendingMoviesResponse._();
+
+//   factory TrendingMoviesResponse.fromJson(List<dynamic> json) => _$TrendingMoviesResponseFromJson(Map<String, dynamic>.from(json));
+
+//   String get fullName {
+//     return <String>[firstName, lastName].join(' ');
+//   }
+// }
+
+// class TrendingMovieInfo {
+
+// }
